e4830a176a6287adb80c37aeed9345e052c432d0
xiyini <q2z@hotmail.com>
2023-02-05 12:41:46 +0400

    Update Object 2 assignment

--- js/079to085/2.js
+++ js/079to085/2.js
@@ -1,20 +1,32 @@
 // Method One
+// Create Your Object Here
+
 let objMethodOne = {
-    property: "Method One",
-};
+  property: "Method One"
+}
 console.log(objMethodOne.property); // "Method One"
 
 // Method Two
+// Create Your Object Here
+
 let objMethodTwo = new Object({
-    property: "Method Two",
-});
+  property: "Method Two"
+})
 console.log(objMethodTwo.property); // "Method Two"
 
 // Method Three
-let objMethodThree = Object.create;
-objMethodThree.property = "Method Three";
+// Create Your Object Here
+
+let objMethodThree = Object.create({
+  property: "Method Three"
+})
 console.log(objMethodThree.property); // "Method Three"
 
 // Method Four
-let objMethodFour = Object.assign({}, { property: "Method Four" });
+// Create Your Object Here
+
+let objMethodFour = Object.assign({
+  property: "Method Four"
+})
 console.log(objMethodFour.property); // "Method Four"
+
