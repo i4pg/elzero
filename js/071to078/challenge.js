7cfb6077fc0b58c42801bc76179db17528bcbbd9
xiyini <q2z@hotmail.com>
2023-02-05 11:58:54 +0400

    Update high order function challenge

--- js/071to078/challenge.js
+++ js/071to078/challenge.js
@@ -1,15 +1,12 @@
 let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
 
-let isOne = (str) => str.length > 1 ? false : true;
-let removeChar = (str) => isOne(str) ? str : str[0]
-let swapUnder = (str) => str === "_" ? " " : str
-
 let solution = myString
   .split(",")
   .filter(isNaN)
-  .map(removeChar)
-  .map(swapUnder)
+  .map((ele) => ele.split("").pop())
+  .map((ele, i, arr) => i >= (arr.length - true) ? "" : ele)
   .join("")
-  .slice(0, -1)
+  .split("_")
+  .join(" ")
 
 console.log(solution)
