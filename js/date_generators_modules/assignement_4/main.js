00731f43c11bc80ba161de7aa112c880c700adec
xiyini <q2z@hotmail.com>
2023-02-13 20:20:39 +0400

    Add JS Date assignment 4

---
+++ js/date_generators_modules/assignement_4/main.js
@@ -0,0 +1,7 @@
+let bd1 = new Date("Nov 23 1996")
+let bd2 = new Date(1996, 10, 23)
+let bd3 = new Date(96, 10, 23)
+
+console.log(bd1.toString())
+console.log(bd2.toString())
+console.log(bd3.toString())
