c4426796135d6c5a83a5dcdce62aa3f506283548
xiyini <q2z@hotmail.com>
2023-02-13 17:48:27 +0400

    Add JS Date assignment 1

---
+++ js/date_generators_modules/assignement_1/main.js
@@ -0,0 +1,17 @@
+const birthday = new Date("Nov 23 1996")
+
+let today = Date.now()
+let milliseconds = today - birthday
+let seconds = milliseconds / 1000
+let minutes = seconds / 60
+let hours = minutes / 60
+let days = hours / 24
+let months = days / 30
+let years = months / 12
+
+console.log(`${seconds.toFixed()} Seconds`)
+console.log(`${minutes.toFixed()} Minutes`)
+console.log(`${hours.toFixed()} Hours`)
+console.log(`${days.toFixed()} Days`)
+console.log(`${months.toFixed()} Months`)
+console.log(`${years.toFixed()} Years`)
