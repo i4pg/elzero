312d6f5468613ad9cd6ea8bee8b54815a73d5624
xiyini <q2z@hotmail.com>
2023-02-15 09:39:57 +0400

    Update JS AJAX&JSON assignment 4

---
+++ js/ajax_json/assignement_4/main.js
@@ -0,0 +1,29 @@
+let req = new XMLHttpRequest();
+req.open("GET", "https://api.npoint.io/cd80e157ee21c29e4744")
+req.send()
+setTimeout(() => {
+  if (req.readyState === 4 && req.status === 200) {
+    let mainData = JSON.parse(req.response)
+    let container = document.createElement("div")
+    container.id = "data"
+    mainData.forEach(article => {
+      console.log(article)
+      let div = document.createElement("div")
+      let tit = document.createElement("h2")
+      let bd = document.createElement("p")
+      let ath = document.createElement("p")
+      let ctg = document.createElement("p")
+
+      tit.append(document.createTextNode(article['Title']))
+      bd.append(document.createTextNode(article['Body']))
+      ath.append(document.createTextNode(article['Author']))
+      ctg.append(document.createTextNode(article['Categorey']))
+      div.append(tit)
+      div.append(bd)
+      div.append(ath)
+      div.append(ctg)
+      container.append(div)
+    });
+    document.body.append(container)
+  }
+}, 1000);
